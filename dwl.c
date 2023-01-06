c321a1e1a2903214f38359a9f26d149a785061e2
Mehran <mehranhn@protonmail.com>
2023-01-06 23:15:38 +0330

    patch attachbottom

--- dwl.c
+++ dwl.c
@@ -1516,7 +1516,15 @@ mapnotify(struct wl_listener *listener, void *data)
 	c->geom.height += 2 * c->bw;
 
 	/* Insert this client into client lists. */
-	wl_list_insert(&clients, &c->link);
+#if PATCHATTACHBOTTOM
+	if (clients.prev)
+		// tile at the bottom
+		wl_list_insert(clients.prev, &c->link);
+	else
+		wl_list_insert(&clients, &c->link);
+#else
+    wl_list_insert(&clients, &c->link);
+#endif
 	wl_list_insert(&fstack, &c->flink);
 
 	/* Set initial monitor, tags, floating status, and focus:
