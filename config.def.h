75863b9f4bcef7b18b1cbfb68d919aa8667c53c8
Mehran <mehranhn@protonmail.com>
2023-01-07 02:02:28 +0330

    patch cursortheme

--- config.def.h
+++ config.def.h
@@ -15,6 +15,8 @@ static const unsigned int gappov           = 10; /* vert outer gap between windo
 static const float rootcolor[]             = {0.3, 0.3, 0.3, 1.0};
 static const float bordercolor[]           = {0.5, 0.5, 0.5, 1.0};
 static const float focuscolor[]            = {1.0, 0.0, 0.0, 1.0};
+static const char *cursortheme             = NULL; /* theme from /usr/share/cursors/xorg-x11 */
+static const unsigned int cursorsize       = 24;
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0};
 
